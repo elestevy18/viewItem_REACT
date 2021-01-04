bde3ec413de0e8470b3870333b833e12fc58143f
Kevin Jimenez <elestevy@gmail.com>
2021-01-04 13:36:22 -0800

    Initial commit

--- App.js
+++ App.js
@@ -1,12 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, image} from 'react-native';
+import colors from "../viewimage/colors"
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View
+    style={styles.container}>
+      <View style ={styles.closedIcon}></View>
+      <View style ={styles.deleteIcon}></View>
+
+    <Image 
+    resizeMode = "contain"
+    style={styles.image}
+    source={require("../viewimage/assets/chair.jpg")}></Image>
     </View>
   );
 }
@@ -17,5 +24,31 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+
+  },
+  closedIcon: {
+    width: 50 ,
+    height: 50,
+    backgroundColor: colors.primary,
+    position: 'absolute',
+    top: 40,
+    left: 30,
+  },
+  deleteIcon: {
+    width: 50 ,
+    height: 50,
+    backgroundColor: colors.secondary,
+    position: 'absolute',
+    top: 40,
+    right: 30,
+  },
+  container: {
+    backgroundColor: "#000",
+    flex: 1,
+
   },
 });
